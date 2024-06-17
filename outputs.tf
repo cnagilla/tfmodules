7fb7b7c9cac9ef142c23c63f7e6b79b3183fb5f1
nagillachan <47925948+nagillachan@users.noreply.github.com>
2024-06-17 16:51:07 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "aks_name" {
   description = "The name of the AKS cluster"
   value       = module.aks.aks_name
 }
+
+output "subnet_id" {
+  description = "The ID of the Subnet"
+  value       = module.network.subnet_id
+}
