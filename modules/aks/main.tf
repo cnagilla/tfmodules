ac52e156a68c2cc3b064ef8715b9cb1fd2b902cf
nagillachan <47925948+nagillachan@users.noreply.github.com>
2024-06-17 16:53:21 +0530

    Update main.tf

--- modules/aks/main.tf
+++ modules/aks/main.tf
@@ -1,25 +1,36 @@
-resource "azurerm_kubernetes_cluster" "aks" {
-  name                = var.aks_name
-  location            = var.location
-  resource_group_name = var.resource_group_name
-  dns_prefix          = var.aks_name
+provider "azurerm" {
+  features {}
+}
 
-  default_node_pool {
-    name       = "default"
-    node_count = var.node_count
-    vm_size    = var.node_vm_size
-  }
+resource "azurerm_resource_group" "main" {
+  name     = var.resource_group_name
+  location = var.location
+}
 
-  identity {
-    type = "SystemAssigned"
-  }
+module "network" {
+  source              = "./modules/network"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  vnet_name           = var.vnet_name
+  subnet_name         = var.subnet_name
+  address_space       = var.address_space
+  subnet_prefix       = var.subnet_prefix
+}
 
-  network_profile {
-    network_plugin    = "azure"
-    load_balancer_sku = "standard"
-  }
+module "aks" {
+  source              = "./modules/aks"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  aks_name            = var.aks_name
+  node_count          = var.node_count
+  node_vm_size        = var.node_vm_size
+  subnet_id           = module.network.subnet_id
+}
 
-  tags = {
-    Environment = "Development"
-  }
+output "aks_name" {
+  value = module.aks.aks_name
+}
+
+output "subnet_id" {
+  value = module.network.subnet_id
 }
