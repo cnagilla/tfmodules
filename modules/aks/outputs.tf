e1357044b8e9ee5058d0690407b2a3f892c19f17
nagillachan <47925948+nagillachan@users.noreply.github.com>
2024-06-17 16:53:40 +0530

    Update outputs.tf

--- modules/aks/outputs.tf
+++ modules/aks/outputs.tf
@@ -1,4 +1,9 @@
 output "aks_name" {
   description = "The name of the AKS cluster"
-  value       = azurerm_kubernetes_cluster.aks.name
+  value       = module.aks.aks_name
+}
+
+output "subnet_id" {
+  description = "The ID of the Subnet"
+  value       = module.network.subnet_id
 }
