2c9b4c76b2097f470ff57b088370cde2b48542ae
nagillachan <47925948+nagillachan@users.noreply.github.com>
2024-06-17 13:12:17 +0530

    Create main.tf

---
+++ modules/aks/main.tf
@@ -0,0 +1,25 @@
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = var.aks_name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  dns_prefix          = var.aks_name
+
+  default_node_pool {
+    name       = "default"
+    node_count = var.node_count
+    vm_size    = var.node_vm_size
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  network_profile {
+    network_plugin    = "azure"
+    load_balancer_sku = "standard"
+  }
+
+  tags = {
+    Environment = "Development"
+  }
+}
