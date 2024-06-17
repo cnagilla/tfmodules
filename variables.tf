d98edad3bc59024f5b215e79e3dc85f10daca663
nagillachan <47925948+nagillachan@users.noreply.github.com>
2024-06-17 16:50:36 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -10,6 +10,30 @@ variable "location" {
   default     = "East US"
 }
 
+variable "vnet_name" {
+  description = "The name of the Virtual Network"
+  type        = string
+  default     = "myVNet"
+}
+
+variable "subnet_name" {
+  description = "The name of the Subnet"
+  type        = string
+  default     = "mySubnet"
+}
+
+variable "address_space" {
+  description = "The address space for the Virtual Network"
+  type        = list(string)
+  default     = ["10.0.0.0/16"]
+}
+
+variable "subnet_prefix" {
+  description = "The address prefix for the Subnet"
+  type        = string
+  default     = "10.0.1.0/24"
+}
+
 variable "aks_name" {
   description = "The name of the AKS cluster"
   type        = string
